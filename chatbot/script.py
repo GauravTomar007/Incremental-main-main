37dfb888c40ca226406bc339962c4238d38a774e
Gaurav tomar <kumar.grv9711@gmail.com>
2026-08-02 17:10:34 +0000

    Final commit signing Off

---
+++ chatbot/script.py
@@ -0,0 +1,15 @@
+from chatbot.src.chat_agent import create_education_agent
+
+# create object 
+agent = create_education_agent()
+
+def run_chatbot(question):
+
+    response = agent.invoke(
+        {
+            "messages": [{"role": "user", "content": question}]
+        }
+    )
+
+    final_msg = response["messages"][-1].content
+    return (final_msg[0]["text"])
